84e243b6de74b75e5801324babb84d823a0c9166
ahmiiidev <123650326+ahmiiidev@users.noreply.github.com>
2024-07-17 00:39:36 -0700

    Add files via upload

---
+++ follow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+    public float minimumdistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,target.position,speed * Time.deltaTime);
+
+        
+    }
+}
